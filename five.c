dbf558c5e3139da3bf5f4850951dfd1fe0ab13f6
ragavanrocky <52520783+ragavanrocky@users.noreply.github.com>
2019-07-11 17:43:29 +0530

    Update five.c

--- five.c
+++ five.c
@@ -2,8 +2,8 @@
 void main()
 {
   int a,b,c;
-  scanf("%d,%d,%d",&a,&b,&c);
-  if(a>b||a>c)
+  scanf("%d, %d, %d",&a,&b,&c);
+  if((a>b)&&(a>c))
     printf("%d",a);
   else if(b>c)
     printf("%d",b);
